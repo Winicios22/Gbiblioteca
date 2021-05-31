fdfff726a66bb901e3f7a80e4507285dc59d425e
Winicios22 <winiciosrq02@gmail.com>
2021-05-31 05:42:42 -0300

    seleção de livro

--- selecionarlivro.cpp
+++ selecionarlivro.cpp
@@ -1,5 +1,8 @@
 #include "selecionarlivro.h"
 #include "ui_selecionarlivro.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 SelecionarLivro::SelecionarLivro(QWidget *parent) :
     QDialog(parent),
@@ -12,3 +15,19 @@ SelecionarLivro::~SelecionarLivro()
 {
     delete ui;
 }
+
+void SelecionarLivro::on_pushButton_2_clicked()
+{
+    QString local="C:/Users/Winicios/Documents/Bear/Bear_Code/Gbiblioteca/arquivos do programa";
+    QString nome="Livros.txt";
+    QFile arquivo(local+nome);
+    if(!arquivo.open(QFile::ReadOnly|QFile::Text)){
+        QMessageBox::warning(this,"ERRO","Erro ao abrir o arquivo");
+    }
+    QTextStream saida(&arquivo);
+    QString texto=saida.readAll();
+    ui->plainTextEdit->setPlainText(texto);
+    arquivo.flush();
+    arquivo.close();
+}
+
--- selecionarlivro.h
+++ selecionarlivro.h
@@ -15,6 +15,9 @@ public:
     explicit SelecionarLivro(QWidget *parent = nullptr);
     ~SelecionarLivro();
 
+private slots:
+    void on_pushButton_2_clicked();
+
 private:
     Ui::SelecionarLivro *ui;
 };
