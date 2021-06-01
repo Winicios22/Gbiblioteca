d47b71600d045054a6a852d0fe8d37b40d5327ab
Winicios22 <winiciosrq02@gmail.com>
2021-06-01 00:20:30 -0300

    seleção de livros

--- teladepedidos.cpp
+++ teladepedidos.cpp
@@ -16,7 +16,17 @@ TelaDePedidos::~TelaDePedidos()
 
 void TelaDePedidos::on_pushButton_clicked()
 {
-    QString nomeDoLivro;
+    QString local="C:/Users/Winicios/Documents/Bear/Bear_Code/Gbiblioteca/arquivos do programa";
+    QString nome="Pedidos.txt";
+    QFile arquivo(local+nome);
+    if(!arquivo.open(QFile::ReadOnly|QFile::Text)){
+        QMessageBox::warning(this,"ERRO","Erro ao abrir o arquivo");
+    }
+    QTextStream saida(&arquivo);
+    QString texto=saida.readAll();
+    ui->plainTextEdit->setPlainText(texto);
+    arquivo.flush();
+    arquivo.close();
 
 }
 
